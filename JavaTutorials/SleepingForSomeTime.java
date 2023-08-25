9552271372b15f0d4c59aa3a18f84ce5b2ff1f95
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-25 21:57:29 +0200

    Sleeping for some time is checked.

--- JavaTutorials/SleepingForSomeTime.java
+++ JavaTutorials/SleepingForSomeTime.java
@@ -1,2 +1,15 @@
-package JavaTutorials;public class SleepingForSomeTime {
+package JavaTutorials;
+
+import java.util.Date;
+
+public class SleepingForSomeTime {
+    public static void main(String args[]) {
+        try {
+            System.out.println(new Date( ) + "\n");
+            Thread.sleep(5*60*10);
+            System.out.println(new Date( ) + "\n");
+        } catch (Exception e) {
+            System.out.println("Got an exception!");
+        }
+    }
 }
