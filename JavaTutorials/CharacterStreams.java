a2ab34e1f8866a59435b7d4f69549aae3afe80b6
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-28 19:01:34 +0200

    Character streams with FileReader is checked.

---
+++ JavaTutorials/CharacterStreams.java
@@ -0,0 +1,29 @@
+package JavaTutorials;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharacterStreams {
+    public static void main(String args[]) throws IOException {
+        FileReader in = null;
+        FileWriter out = null;
+
+        try {
+            in = new FileReader("input.txt");
+            out = new FileWriter("output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        }finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
