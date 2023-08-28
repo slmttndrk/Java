eebbab6f52af0cc7c39edffff85736a606d4e1b4
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-28 19:17:30 +0200

    Listing directory with list() is checked.

---
+++ JavaTutorials/ListDirectory.java
@@ -0,0 +1,27 @@
+package JavaTutorials;
+
+import java.io.File;
+
+public class ListDirectory {
+    public static void main(String[] args) {
+        File file = null;
+        String[] paths;
+
+        try {
+            // create new file object
+            file = new File("/tmp");
+
+            // array of files and directory
+            paths = file.list();
+
+            // for each name in the path array
+            for(String path:paths) {
+                // prints filename and directory name
+                System.out.println(path);
+            }
+        } catch (Exception e) {
+            // if any error occurs
+            e.printStackTrace();
+        }
+    }
+}
