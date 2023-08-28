02d9b931161794e7a54b9865e1f26d90c081b9d2
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-28 19:15:02 +0200

    Creating directory with mkdirs() is checked.

---
+++ JavaTutorials/CreateDirectory.java
@@ -0,0 +1,16 @@
+package JavaTutorials;
+
+import java.io.File;
+
+// Java automatically takes care of path separators on UNIX and Windows as per conventions. If
+// you use a forward slash (/) on a Windows version of Java, the path will still resolve correctly.
+
+public class CreateDirectory {
+    public static void main(String args[]) {
+        String dirname = "/tmp/user/java/bin";
+        File d = new File(dirname);
+
+        // Create directory now.
+        d.mkdirs();
+    }
+}
