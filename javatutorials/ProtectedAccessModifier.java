adc36d05c4f8b52693b5af82e32fc2143dcc396d
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-29 12:43:59 +0200

    Protected access modifier is checked.

---
+++ javatutorials/ProtectedAccessModifier.java
@@ -0,0 +1,14 @@
+package javatutorials;
+
+class AudioPlayer {
+    protected void openSpeaker(String s) {
+        // implementation details
+    }
+}
+
+class ProtectedAccessModifier extends AudioPlayer {
+    @Override
+    protected void openSpeaker(String s) {
+        // implementation details
+    }
+}
