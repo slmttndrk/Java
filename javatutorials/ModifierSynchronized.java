4402dc1f09a1993c5eee58cfd60202dec03f4878
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-30 20:50:48 +0200

    Synchronized modifier is checked.

---
+++ javatutorials/ModifierSynchronized.java
@@ -0,0 +1,13 @@
+package javatutorials;
+
+/**
+ * The Synchronized Modifier
+ * The synchronized keyword used to indicate that a method can be accessed by only one thread at a time.
+ * The synchronized modifier can be applied with any of the four access level modifiers.
+ * */
+
+public class ModifierSynchronized {
+    public synchronized void showDetails() {
+        // TODO
+    }
+}
