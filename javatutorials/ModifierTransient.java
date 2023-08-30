e14eace97486f82b52c28f9f7696ac8fffb280cb
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-30 20:52:12 +0200

    Synchronized modifier is checked.

---
+++ javatutorials/ModifierTransient.java
@@ -0,0 +1,14 @@
+package javatutorials;
+
+/**
+ * The Transient Modifier
+ * An instance variable is marked transient to indicate the JVM to skip the particular variable when serializing
+ * the object containing it.
+ * This modifier is included in the statement that creates the variable, preceding the class or data type of
+ * the variable.
+ * */
+
+public class ModifierTransient {
+    public transient int limit = 55;   // will not persist
+    public int b;   // will persist
+}
