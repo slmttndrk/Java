c44ba85837e92beadf2751d84df35d5d499f23a6
Selamettin Dirik <selamettinn.95@gmail.com>
2023-08-31 13:02:45 +0200

    Implements keyword is checked.

---
+++ javatutorials/KeywordImplements.java
@@ -0,0 +1,13 @@
+package javatutorials;
+
+/**
+ * The implements keyword is used to get the IS-A relationship.
+ * Generally, the implements keyword is used with classes to inherit the properties of an interface.
+ * Interfaces can never be extended by a class.
+ * */
+
+interface AnInterface{
+}
+
+public class KeywordImplements implements AnInterface{
+}
