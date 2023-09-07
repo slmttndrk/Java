d8a30b7f69f371b85a82cbb9fee61fcffde9c40c
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-07 23:10:07 +0200

    Multithreading with Interthread Communication is checked.

---
+++ javatutorials/MultiThreadingInterthreadCommunication.java
@@ -0,0 +1,79 @@
+package javatutorials;
+
+/**
+ * Interthread communication is important when you develop an application where two or more threads exchange some
+   information. There are three simple methods and a little trick which makes thread communication possible.
+ * These methods have been implemented as final methods in Object, so they are available in all the classes.
+ * All three methods can be called only from within a synchronized context.
+ * */
+
+class Chat {
+    boolean flag = false;
+
+    public synchronized void Question(String msg) {
+        if (flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(msg);
+        flag = true;
+        notify();
+    }
+
+    public synchronized void Answer(String msg) {
+        if (!flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(msg);
+        flag = false;
+        notify();
+    }
+}
+
+class T1 implements Runnable {
+    Chat m;
+    String[] s1 = { "Hi", "How are you ?", "I am also doing fine!" };
+
+    public T1(Chat m1) {
+        this.m = m1;
+        new Thread(this, "Question").start();
+    }
+
+    public void run() {
+        for (int i = 0; i < s1.length; i++) {
+            m.Question(s1[i]);
+        }
+    }
+}
+
+class T2 implements Runnable {
+    Chat m;
+    String[] s2 = { "Hi", "I am good, what about you?", "Great!" };
+
+    public T2(Chat m2) {
+        this.m = m2;
+        new Thread(this, "Answer").start();
+    }
+
+    public void run() {
+        for (int i = 0; i < s2.length; i++) {
+            m.Answer(s2[i]);
+        }
+    }
+}
+
+public class MultiThreadingInterthreadCommunication {
+    public static void main(String[] args) {
+        Chat m = new Chat();
+        new T1(m);
+        new T2(m);
+    }
+}
