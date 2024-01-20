d4d84b8c6ad770dc8c4bf33a91b1a96ddde81b68
Selamettin Dirik <selamettinn.95@gmail.com>
2024-01-20 21:56:07 +0100

    Checked Searching Algorithm.

---
+++ datastructuresalgorithms/algorithms/Search.java
@@ -0,0 +1,22 @@
+package datastructuresalgorithms.algorithms;
+
+/***
+ * The searching algorithms are used to search or find one or more than one element from a dataset.
+ * These type of algorithms are used to find elements from a specific data structures.
+ * Searching may be sequential or not. If the data in the dataset are random, then we need to use
+ * sequential searching. Otherwise, we can use other different techniques to reduce the complexity.
+ * Search algorithms can be classified based on their mechanism of searching into three types of
+ * algorithms: linear, binary, and hashing
+ *
+ * Types of Searches:
+ *     - Sequential Search: In this, the list or array is traversed sequentially and every element
+ *       is checked. For example: Linear Search
+ *     - Interval Search: These algorithms are specifically designed for searching in sorted
+ *       data-structures. These type of searching algorithms are much more efficient than Linear
+ *       Search as they repeatedly target the center of the search structure and divide the search
+ *       space in half. For Example: Binary Search
+ *
+ */
+
+public class Search {
+}
